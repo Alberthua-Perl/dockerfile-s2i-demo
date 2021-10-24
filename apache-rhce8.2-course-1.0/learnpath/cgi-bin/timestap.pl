58b57eb6dd35d822a7dbf8baeacd2d2c4febb999
Alberthua <hualongfeiyyy@163.com>
2021-10-24 08:46:54 +0800

    Delete apache-rhce8.2-course-1.0/learnpath directory

--- apache-rhce8.2-course-1.0/learnpath/cgi-bin/timestap.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-use warnings;
-use strict;
-use CGI qw(:standard);
-
-print header;
-my $now_time = localtime();
-print "<b>Hello, CGI using Perl!</b><br><br>It's $now_time now<br>";
